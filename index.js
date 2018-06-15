513e0aead3f509e03243c933236e051b36851254
Micke B <mikael.berglund@student.kyh.se>
2018-06-15 12:17:50 +0200

    fsafs

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var connection = mysql.createConnection({
   host     : 'crockett.highstone.biz',
   user     : 'portfolio',
   password : 'Portfolio123',
-  database : 'kjk_portfolio'
+  database : 'mb_portfolio'
 });
 
 const app = express();
@@ -36,14 +36,14 @@ app.listen(process.env.PORT || port);
 const student = {
   firstname: "Lisa",
   lastname: "LaFlamme",
-  age: 21, 
+  age: 21,
   classes: ["street", "dancehall", "balett"]
   };
-  
+
   const teacher = {
     firstname: "Adrienne",
     lastname: "Picard",
-    age: 21, 
+    age: 21,
     classes: ["street", "dancehall", "balett"]
     };
 
@@ -58,7 +58,7 @@ const capitalize = function (string) {
 
 
   let res;
-  
+
 
 app.get('/student', function (req, res) {
     res.json(student);
@@ -73,16 +73,16 @@ app.get('/comment', function (req, res) {
   // error will be an Error if one occurred during the query
   // results will contain the results of the query
   // fields will contain information about the returned results fields (if any)
-  
+
   res.json(results);
-  
+
 });
 });
 
 app.post('/comment', function (req, res) {
   var query = "INSERT INTO ??(??,??) VALUES (?,?)";
   var inserts = ['comment', 'author', 'body', req.body.author, req.body.body];
-  sql = mysql.format(query, inserts); 
+  sql = mysql.format(query, inserts);
   query = mysql.format(query,inserts);
   connection.query(query,function(err,rows){
     if(err) {
@@ -91,7 +91,7 @@ app.post('/comment', function (req, res) {
         res.json({"Error" : false, "Message" : "Comment Added !"});
     }
 });
-});  
+});
 // todo add classes endpoint
 
 console.log(`App listening on port ${port}`);
